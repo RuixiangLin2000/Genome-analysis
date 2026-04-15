805e292f9fca16f17efef45455d9da5da0373e99
RuixiangLin <xianliang200@gmail.com>
2026-04-15 22:01:38 +0200

    Add Flye assembly batch script for chr3 with absolute paths

---
+++ code/flye_assembly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 12:00:00
+#SBATCH -J flye_chr3
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+module load Flye/2.9.6-GCC-13.3.0
+
+flye \
+  --nano-raw /home/ruixi/Genome-analysis/raw_data/genomics_chr3/chr3_clean_nanopore.fq.gz \
+  --out-dir /home/ruixi/Genome-analysis/analyses/02_assembly/flye_chr3 \
+  --threads 2 \
+  --resume
