1527f457ca1f6509758945a2dbf918eb7f12a7de
RuixiangLin <xianliang200@gmail.com>
2026-04-19 14:55:02 +0200

    Remove --resume flag from Flye assembly script

--- code/flye_assembly.sh
+++ code/flye_assembly.sh
@@ -3,7 +3,7 @@
 #SBATCH -p pelle
 #SBATCH -c 2
 #SBATCH --mem=96G
-#SBATCH -t 48:00:00
+#SBATCH -t 24:00:00
 #SBATCH -J flye_chr3
 #SBATCH --mail-type=ALL
 #SBATCH --output=%x.%j.out
@@ -14,4 +14,4 @@ flye \
   --nano-raw /home/ruixi/Genome-analysis/raw_data/genomics_chr3/chr3_clean_nanopore.fq.gz \
   --out-dir /home/ruixi/Genome-analysis/analyses/02_assembly/flye_chr3 \
   --threads 2 \
-  --resume
+
